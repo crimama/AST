8b73f7b5e1f0f1ccf28a85f333ec7338f0a585e3
crimama2 <crimama2@gmail.com>
2023-03-20 04:24:22 +0000

    add wandb, main

--- train_teacher.py
+++ train_teacher.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 import os 
 import numpy as np 
 import wandb 
+import pandas as pd 
 
 def train(teacher,trainloader,save_dir):
     #! save dir 
@@ -14,17 +15,27 @@ def train(teacher,trainloader,save_dir):
         os.makedirs(save_dir)
     else:
         pass 
-    #! wandb 
     
+    #! wandb 
+    config = pd.Series(dir(c))[pd.Series(dir(c)).apply(lambda x : '__' not in x )].values
+    config_dict = {} 
+    config_dict['class_name'] = save_dir.split('/')[-1]
+    config_dict['Teacher/Student'] = 'Teacher'
+    for i in range(len(config)):
+        config_dict[config[i]] = __import__('config').__dict__[f'{config[i]}']
+    wandb.init(name=save_dir.split('/')[-1],config=config_dict)
+    
+    
+    #! Train 
     optimizer = torch.optim.Adam(teacher.net.parameters(),lr=c.lr,eps=1e-08,weight_decay=1e-5)
     best_loss = np.inf 
     #for epoch in tqdm(range(c.total_epochs)):
-    for epoch in range(c.meta_epochs):
+    for epoch in tqdm(range(c.meta_epochs)):
         teacher.train()
         if c.verbose:
             print(f'\nTrain epoch {epoch}')
             
-        for sub_epoch in tqdm(range(c.sub_epochs)):
+        for sub_epoch in range(c.sub_epochs):
             train_loss = list() 
             for i,data in enumerate(trainloader):
                 optimizer.zero_grad()
@@ -47,10 +58,12 @@ def train(teacher,trainloader,save_dir):
                 trainloader.dataset.init_mode = False
             
             mean_train_loss = np.mean(train_loss)
-        
+
+            #! logging 
             if sub_epoch % 4 == 0:  # and epoch == 0:
                 print('Epoch: {:d}.{:d} \t train loss: {:.4f}'.format(epoch, sub_epoch, mean_train_loss))
-            
+            wandb.log({'Epoch_loss':mean_train_loss})
+
         if best_loss > mean_train_loss:
             best_loss = mean_train_loss
             torch.save(teacher,save_dir + '/teacher_best.pt')
@@ -58,14 +71,20 @@ def train(teacher,trainloader,save_dir):
 
 
 if __name__ == "__main__":
-    dataset_dir = c.dataset_dir
-    class_name = 'bottle'
-    mode = 'feature'
-    train = 'train'
-    trainset = CombiDataset(dataset_dir,class_name,mode,train,device=c.device)
-    testset = CombiDataset(dataset_dir,class_name,mode,traindevice=c.device)
-    trainloader = make_loader(trainset,shuffle=True)
-    testloader = make_loader(testset)
+    all_classes = [d for d in os.listdir(c.dataset_dir) if os.path.isdir(os.path.join(c.dataset_dir, d))]
+    all_classes.remove('.ipynb_checkpoints')
+    all_classes.remove('split_csv')
 
-    teacher = Model().to(c.device)
-    train(teacher,trainloader)
+    dataset_dir = c.dataset_dir
+    mode = 'feature'
+
+    for class_name in all_classes:
+    #for class_name in ['cable']:
+        print(f'\n Class : {class_name}')
+        save_dir = os.path.join('./saved_models',dataset_dir.split('/')[-1],class_name)
+        
+        trainset = CombiDataset(dataset_dir,class_name,mode,'train',device=c.device)
+        trainloader = make_loader(trainset,shuffle=True)
+        
+        teacher = Model().to(c.device)
+        train(teacher,trainloader,save_dir)
