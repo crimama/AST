c00a5048968e4495e224c525592c72a6537d6ffe
crimama <crimama2@gmail.com>
2023-03-29 14:31:11 +0000

    [Update] def eval_pixel

--- eval.py
+++ eval.py
@@ -13,10 +13,10 @@ def eval(teacher,student,testloader):
     y_true = [] 
     for data in testloader:
         #load data 
-        img,label,depth,fg = to_device(data,c.device)
-        if c.use_3D_dataset:
-            fg = dilation(fg, c.dilate_size) if c.dilate_mask else fg 
-            fg_down = downsampling(fg, (c.map_len,c.map_len),bin=False)
+        img,label,gt,depth,fg = to_device(data,c.device)
+        
+        fg = dilation(fg, c.dilate_size) if c.dilate_mask else fg 
+        fg_down = downsampling(fg, (c.map_len,c.map_len),bin=False)
         #inference 
         with torch.no_grad():    
             z_t,jac_t = teacher(img,depth)
@@ -38,4 +38,41 @@ def eval(teacher,student,testloader):
     max_image_auc = roc_auc_score(y_true,max_image_loss)
     
     return mean_image_auc,max_image_auc
+
+
+def eval_pixel(teacher,student,testloader):
+    student.eval()
+    teacher.eval() 
+
+    y_true = []
+    y_pred = [] 
+    for data in testloader:
+        # load data 
+        img,label,gt,depth,fg = to_device(data,c.device)
+        # foreground prepare  
+        fg = dilation(fg, c.dilate_size) if c.dilate_mask else fg 
+        fg_down = downsampling(fg, (c.map_len,c.map_len),bin=False)
+        
+        # inference 
+        with torch.no_grad():
+            z_t,jac_t = teacher(img,depth)
+            z_s,jac_s = student(img,depth)
+        st_pixel = get_st_loss(z_t,z_s,fg_down,per_pixel=True)
+        
+        if c.eval_mask:
+            st_pixel = st_pixel * fg_down[:,0]
+        
+        #localize(image,depth,st_pixel,labels,fg,mask)
+        up = torch.nn.Upsample(size=None,scale_factor=c.depth_len//c.map_len,mode='bicubic',align_corners=False)
+        #up = torch.nn.Upsample(size=1024,mode='bicubic',align_corners=False)
+        st_pixel = up(st_pixel[:,None])
+        
+        y_true.append(gt)
+        y_pred.append(torch.squeeze(st_pixel,dim=1))
+
+    y_true = torch.concat(y_true,dim=0)
+    y_pred = torch.concat(y_pred,dim=0)
+    #auroc eval 
+    auroc = roc_auc_score(t2np(y_true).flatten(),t2np(y_pred).flatten())
+    return auroc 
         
